d5620ab7a3366569ce02a459cd82aac5eddf0795
IlSeul <adolfo.rojas.v@ug.uchile.cl>
2022-09-15 23:37:03 -0300

    git commit

--- Sheep/script.js
+++ Sheep/script.js
@@ -1,11 +1,15 @@
 document.querySelectorAll('.accordion-button').forEach(button => {
     button.addEventListener("click", () => {
       const accordionContent = button.nextElementSibling;
-      button.classList.toggle("accordion-button-active");
-      if (button.classList.contains("accordion-button-active")) {
+      button.classList.toggle("accordion-button.active");
+      if (button.classList.contains("accordion-button.active")) {
         accordionContent.style.maxheight = accordionContent.scrollHeight + "px";
       } else {
         accordionContent.style.maxheight = 0;
       }
     });
   });
+var buttonActive = document.querySelectorAll()
+function resetButton() {
+    
+}
