a786541c9b9e5dd879ea62490767d5863b634a52
IlSeul <adolfo.rojas.v@ug.uchile.cl>
2022-09-15 23:31:44 -0300

    git commit

---
+++ Sheep/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener("click", () => {
+      const accordionContent = button.nextElementSibling;
+      button.classList.toggle("accordion-button-active");
+      if (button.classList.contains("accordion-button-active")) {
+        accordionContent.style.maxheight = accordionContent.scrollHeight + "px";
+      } else {
+        accordionContent.style.maxheight = 0;
+      }
+    });
+  });
