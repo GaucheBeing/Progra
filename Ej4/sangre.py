8ab078f1a55d5a207570eb8efdb0d644046305ec
IlSeul <adolfo.rojas.v@ug.uchile.cl>
2022-09-26 23:59:52 -0300

    git commit

--- Ej4/sangre.py
+++ Ej4/sangre.py
@@ -32,19 +32,21 @@ LS2 = lista(SS1, lista(SS2, lista(SS3, listaVacia)))
 
 # universal: lista -> int
 # Función: dada una lista de Sangre, cuente cuanta sangre hay del tipo O con factor RH negativo
-# Ej: 
+# Ej: universal(LS1) y universal(LS2) han de retornar 2 y 0 respectivamente
 def universal(listaS):
        assert esLista(listaS)
        def _universal(listaS):
+              # caso base
               if listaS == None:
                      return 0
-              else:   
-                     if cabeza(listaS) == "O" and cabeza(listaS) == "-":
-                            1
-                            return _universal(cola(listaS))
+              else:
+                     # chequear los atributos 'tipo' y 'rh' de elementos (Sangre) de una lista recursivamente
+                     elementS = cabeza(listaS)
+                     if elementS.tipo == 'O' and elementS.rh == "-":
+                            return 1 + _universal(cola(listaS))
                      else:
-                            0
-                            return _universal(cola(listaS))
+                            return 0 + _universal(cola(listaS))
+       # correr la subfunción hasta terminar la lista dada
        return _universal(listaS)
 # Test:
 assert universal(LS1) == 2
@@ -52,13 +54,27 @@ assert universal(LS2) == 0
 
 # soloTipo: lista str -> lista 
 # Función: dada una lista de Sangre y un tipo, entregar una lista que solo contenga Sangre del tipo indicado
-# Ej:
-# def soloTipo(listaS, t):
-#        assert esLista(listaS) and type(t) == str
-#        assert t == "A" or t == "B" or t == "AB" or t == "O", \
-#        "Sólo puedes escoger entre tipos A, B, AB u O de sangre"
-#        def _soloTipo(listaS, t)       
-#               if cabeza(listaS)
-
-#               return None
-#        return _soloTipo(listaS, t)
+# Ej1: soloTipo(LS1, "AB") debería entregar una lista con Sangre de códigos 460 y 809
+# Ej2: soloTipo(LS1, "A") entrega una lista con Sangre de código 127
+# Ej3: soloTipo(LS2, "A") entrega nada (la lista vacía)
+def soloTipo(listaS, t):
+       assert esLista(listaS) and type(t) == str
+       assert t == "A" or t == "B" or t == "AB" or t == "O", \
+       "Sólo puedes escoger entre tipos A, B, AB u O de sangre"
+       def _soloTipo(listaS, t):
+              if listaS == None:
+                     return None
+              else:
+                     # chequear si el atributo 'tipo' de un elemento perteneciente a la lista es igual a t
+                     # si la sangre es en efecto tipo *t*, agregar este elemento a una nueva lista
+                     # en caso contrario proceder a analizar el siguiente elemento de la lista proveída hasta finalizar la recursión
+                     elementS = cabeza(listaS)
+                     if elementS.tipo == t:
+                            return lista(elementS, _soloTipo(cola(listaS), t))
+                     else:
+                            return _soloTipo(cola(listaS), t)
+       return _soloTipo(listaS, t)
+# Test:
+assert soloTipo(LS1, "AB") == lista(Sangre(460, 'AB', '-') , lista(Sangre(809, 'AB', '+'), None))
+assert soloTipo(LS1, "A") == lista(Sangre(127, 'A', '+'), None)
+assert soloTipo(LS2, "A") == None
