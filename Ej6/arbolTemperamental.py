9d9078fc9e05b71d56d30d8fa197353e0a64e20b
Adolfo <adolfo.rojas.v@ug.uchile.cl>
2022-10-18 00:58:16 -0300

    iniciar Ej6

--- Ej6/arbolTemperamental.py
+++ Ej6/arbolTemperamental.py
@@ -1,7 +1,6 @@
 from arbol import *
 
 
-
 # ---------------------------------
 # AB's temperamentales de ejemplo
 
@@ -26,6 +25,50 @@ AB3 = AB(50,
           AB(40, AB(20, arbolVacio, arbolVacio), 
                  AB(88, arbolVacio, arbolVacio)))
 
+# validarTemperamento: AB -> bool
+# Función: dado un árbol (binario) validar si este es temperamental o no
+# Ej: validarTemperamento(ABT1) => True y para ABT2 también excepto para AB3  
 
+def validarTemperamento(A):
+    assert esAB(A)
+    if A == None:
+        return True
+    else:
+        # caso None
+        if A.valor == None:
+            return True
+        # caso hoja
+        elif A.izq == None == A.der:
+            return True
+        # caso 1 rama izq
+        elif A.izq < A.valor and A.der == None:
+            return validarTemperamento(A.izq)
+        # caso 1 rama der
+        elif A.izq == None and A.der < A.valor:
+            return validarTemperamento(A.der)
+        # caso nodo ambas ramas
+        if A.izq < A.valor and A.der < A.valor:
+            return validarTemperamento(A.izq) and validarTemperamento(A.der)
+        else: return False
 
+#        if A.valor == None
+#            return True
+#        elif A.izq == None:
+#            if A.der == None:
+#                return True
+#            else:
+#                if A.der < A.valor:
+#                    return validarTemperamento(A.der)
+#                else: return False
+#        elif A.der == None:
+#            if A.izq < A.valor:
+#                return validarTemperamento(A.izq)
+#            else: return False
+#        elif A.izq < A.valor and A.der < A.valor:
+#            return validarTemperamento(A.izq) and validarTemperamento(A.der)
+#        else: return False
 
+# Testing
+assert validarTemperamento(ABT1)
+assert validarTemperamento(ABT2)
+assert not validarTemperamento(AB3)
